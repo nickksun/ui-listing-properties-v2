56d4affd4ca6a2e9e27b824d16338dd08f45ad73
Yiqi Yu <yuyiqiwendy@gmail.com>
2021-01-03 20:05:40 +1100

    Add card status constants

---
+++ src/constants/cardStatusConstants.ts
@@ -0,0 +1,9 @@
+const SAVED_PROPERTY = "Saved";
+const SAVE_PROPERTY="Add"
+const REMOVE_PROPERTY = "Remove";
+
+export const CARD_STATUS = {
+    SAVE_PROPERTY,
+    SAVED_PROPERTY,
+    REMOVE_PROPERTY
+}
